54842f5d217dd2658f9819e8404a80411dd3af07
Kevin Moreno <kevinantonioc13@gmail.com>
2023-09-02 19:24:52 -0600

    deploy4

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,23 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {//para servir el mismo dominio
-    proxy: {
-      '/socket.io': {
-        target: 'http://localhost:3000',
-        ws: true
-      }
-    },
-    base: '/tic-tac-toe-online/'
+  server: {
+    base: '/tic-tac-toe-online/',
+    http2: true,
   },
   build: {
-    //para redirigir las solicitudes a /socket.io al servidor de desarrollo
-    proxy: {
-      '/socket.io': {
-        target: 'http://localhost:3000',
-        ws: true
-      }
-    }
+    base: '/tic-tac-toe-online/',
+    minify: true,
   },
-  base: '/tic-tac-toe-online/'
 })
