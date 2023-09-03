ebfce8563b4b5d33ac4b779021cc6fb2401a825b
Kevin Moreno <kevinantonioc13@gmail.com>
2023-09-02 22:21:44 -0600

    base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tic-tac-toe-online/',
   server: {//para servir el mismo dominio
     proxy: {
       '/socket.io': {
