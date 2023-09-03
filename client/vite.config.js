0c8da5b51b690d719c4c67a76b2e025883fc5daa
Kevin Moreno <kevinantonioc13@gmail.com>
2023-09-02 20:50:03 -0600

    normal

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {//para servir el mismo dominio
     proxy: {
       '/socket.io': {
-        target: 'https://kevg1t.github.io/tic-tac-toe-online',
+        target: 'http://localhost:3000',
         ws: true
       }
     }
