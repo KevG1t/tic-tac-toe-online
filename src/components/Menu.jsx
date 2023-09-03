5b633106bf95d3311e010d800ed0ddca423ea625
Kevin Moreno <kevinantonioc13@gmail.com>
2023-09-03 03:03:55 -0600

    css 2

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 export function Menu () {
     return (
         <section className='board menu'>
-            <i id='x'>❌</i>
-            <i id='o'>⚪</i>
             <h1> Tic Tac Toe</h1>
         <ul className='menu'>
+            <i id='x'>❌</i>
+            <i id='o'>⚪</i>
             <li className='option'>
                 <Link className='link' to='/game'>Jugar</Link>
             </li>
