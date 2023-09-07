9a0f24c6798c05109050858d40c851ccea1764a6
Kevin Moreno <kevinantonioc13@gmail.com>
2023-09-06 19:51:19 -0600

    add server four time

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 //
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'client/dist' // Especifica el directorio de salida como "dist"
-  }
+  plugins: [react()]
 })
