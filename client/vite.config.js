8fe3d8a3949c497f15a6316b6d2b6573df2ef2cc
Kevin Moreno <kevinantonioc13@gmail.com>
2023-09-06 19:40:44 -0600

    add server thirt time

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Especifica el directorio de salida como "dist"
+    outDir: 'client/dist' // Especifica el directorio de salida como "dist"
   }
 })
